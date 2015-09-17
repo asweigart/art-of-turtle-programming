d28870c1fe206c26e6451c1c0e7dbed5c13246fd
Al Sweigart <asweigart@gmail.com>
2015-09-17 16:43:49 -0700

    Adding Ulam Spiral program.

---
+++ simple/ulam_spiral.py
@@ -0,0 +1,42 @@
+from turtle import *
+import math
+
+tracer(1000, 0)
+
+SPACING = 5
+
+bgcolor('#353337')
+pencolor('#CCCCCC')
+
+def amount_of_divisors(number):
+    total = 0
+    for i in range(2, int(math.sqrt(number)) + 1):
+        if number % i == 0:
+            total += 1
+    return total
+
+penup() # (!) Try commenting this line out.
+forward(SPACING) # 1 is not prime, so skip
+left(90)
+dot(2) # 2 is prime, so make a dot
+forward(SPACING)
+left(90)
+
+current_number = 3
+length = 3
+while current_number < 40000:
+    for i in range(2):
+        for j in range(length):
+            divs = amount_of_divisors(current_number)
+            current_number += 1
+
+            if divs > 0:
+                dot((int(math.sqrt(divs)) + 1) * 2, '#76b7eb')
+            #else:
+            #    dot(4, '#ffa25e')
+            forward(SPACING)
+        left(90)
+    length += 1
+
+update()
+exitonclick()
