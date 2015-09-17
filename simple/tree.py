94e8b61524d1dd3c28dea9eda653b31abe3fb6bd
Al Sweigart <asweigart@gmail.com>
2015-09-16 22:40:31 -0700

    Adding screenshot of sierpinski triangle.

--- simple/tree.py
+++ simple/tree.py
@@ -1,27 +1,23 @@
 from turtle import *
+import random
+import time
 
-tracer(1, 0)
+tracer(1000, 0)
 
-# try changing these values and looking at the results
-LEFT_ANGLE = 30
-RIGHT_ANGLE = 20
-LEFT_DECREASE = 7
-RIGHT_DECREASE = 11
-
-def tree(x, y, direction, branch_length):
+def branch(x, y, direction, branch_length):
     # if the branch is too small, just quit
     if branch_length < 5:
         return
 
     # draw the trunk
-    pensize(max(branch_length / 8.0, 1))
+    pensize(max(branch_length / 7.0, 1))
     forward(branch_length)
 
     # draw the two branches, which are fractal trees
     left(LEFT_ANGLE)
-    tree(xcor(), ycor(), heading(), branch_length - LEFT_DECREASE)
+    branch(xcor(), ycor(), heading(), branch_length - LEFT_DECREASE)
     right(LEFT_ANGLE + RIGHT_ANGLE)
-    tree(xcor(), ycor(), heading(), branch_length - RIGHT_DECREASE)
+    branch(xcor(), ycor(), heading(), branch_length - RIGHT_DECREASE)
     left(RIGHT_ANGLE)
 
     # return back to the starting point
@@ -29,14 +25,26 @@ def tree(x, y, direction, branch_length):
     goto(x, y)
     setheading(direction)
     pendown()
-        
-# move the turtle to the starting point at the root
-penup()
-goto(0, -310)
-setheading(90)
-pendown()
 
-# draw the first branch
-tree(xcor(), ycor(), heading(), 100)
-update()
+def draw_tree(x, y, direction, seed):
+    global LEFT_ANGLE, RIGHT_ANGLE, LEFT_DECREASE, RIGHT_DECREASE
+
+    # go to the starting point
+    penup()
+    goto(x, y)
+    setheading(direction)
+    pendown()
+    
+    # try changing these values and looking at the results
+    random.seed(seed)
+    LEFT_ANGLE     = random.randint(20,  40)
+    RIGHT_ANGLE    = random.randint(20,  40)
+    LEFT_DECREASE  = random.randint( 6,  15)
+    RIGHT_DECREASE = random.randint( 6,  15)
+    START_SIZE     = random.randint(60, 110)
+
+    branch(x, y, direction, START_SIZE)
+    update()
+
+draw_tree(0, -310, 90, 123)
 exitonclick()
